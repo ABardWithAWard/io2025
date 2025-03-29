cc20e23a1aad7036503a1d3aa6b021261ed7b120
teYdemaNtoN <florson2002@gmail.com>
2025-03-29 22:45:13 +0100

    Admin panel, narazie bez css i navbara

--- ocr/application/admin.py
+++ ocr/application/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import fileLimit, dataLimit, blockList
+
+class limitAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(dataLimit, limitAdmin)
+admin.site.register(fileLimit, limitAdmin)
+admin.site.register(blockList)
