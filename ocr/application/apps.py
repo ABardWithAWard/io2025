e1e824deed1246631dee5aebea267edb9989f1d7
teYdemaNtoN <florson2002@gmail.com>
2025-03-29 22:58:38 +0100

    Admin dodaje sie automatycznie na starcie aplikacji

--- ocr/application/apps.py
+++ ocr/application/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 from django.db.utils import OperationalError, ProgrammingError
+from django.db.utils import OperationalError
+from django.contrib.auth import get_user_model
 
 
 class ApplicationConfig(AppConfig):
@@ -17,5 +19,9 @@ class ApplicationConfig(AppConfig):
             if not fileLimit.objects.exists():
                 fileLimit.objects.create(value=0)
 
+            User = get_user_model()
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser("admin", "admin@example.com", "admin")
+
         except (OperationalError, ProgrammingError) as e:
-            print(f"Błąd podczas tworzenia rekordów: {e}")
+            print(f"Error: {e}")
