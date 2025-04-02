e494eaf865f46a062cb8588d0a9a68badf4b43a2
Mateusz Wątły <95935287+ABardWithAWard@users.noreply.github.com>
2025-04-02 22:15:18 +0200

    Added function which prevents uploading non image files. Might change allowed extensions if it is impossible to feed them all to model later on.

--- ocr/application/forms.py
+++ ocr/application/forms.py
@@ -1,5 +1,18 @@
 from django import forms
+from django.core.exceptions import ValidationError
+import os
 
+def validate_image_file(value):
+    allowed_extensions = ['.png', '.jpg', '.jpeg', '.bmp', '.gif', '.tiff', '.webp']
+
+    ext = os.path.splitext(value.name)[1].lower()
+    if ext not in allowed_extensions:
+        raise ValidationError('Only image files are allowed.')
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(
+        validators=[validate_image_file],
+        widget=forms.FileInput(attrs={
+            'accept': '.png,.jpg,.jpeg,.bmp,.gif,.tiff,.webp'
+        })
+    )
