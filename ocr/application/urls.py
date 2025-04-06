f7c9b93f273d917e856a8c9472cb56c18cc26424
teYdemaNtoN <florson2002@gmail.com>
2025-04-06 11:37:59 +0200

    Logout, change password, contact page, navbar tag links

--- ocr/application/urls.py
+++ ocr/application/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'application'
+
 urlpatterns = [
     path('', views.upload_file, name='index'),
     path("api/files", views.get_files, name="get_files"),
+    path('contact/', views.contact, name="contact"),
 ]
 
