d52e4cc2afefe6a8189e8ab0651e2db8fe506e54
Mateusz Wątły <95935287+ABardWithAWard@users.noreply.github.com>
2025-04-13 15:39:10 +0200

    Ready to merge. Contact does not work. Google in progress.

--- ocr/application/urls.py
+++ ocr/application/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 from . import login
 
+app_name = 'application'
+
 urlpatterns = [
     path('', views.upload_file, name='index'),
     path("api/files", views.get_files, name="get_files"),
-    path('contact/', views.contact, name="contact"),
-    # Login-related URLs
     path('auth/login/', login.handle_login, name='handle_login'),
     path('auth/register/', login.handle_register, name='handle_register'),
     path('auth/logout/', login.handle_logout, name='logout'),
