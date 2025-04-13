a66af82e3587af611bed3892db97c17284d6e5ca
kon-mtal <konradmstlr@gmail.com>
2025-04-13 17:46:14 +0200

    Implemented interaction between Django and TrOCR

--- ocr/application/services.py
+++ ocr/application/services.py
@@ -3,7 +3,6 @@ import time
 from django.core.files.storage import FileSystemStorage
 
 from application.model.trocr import TrOCR
-from model import trocr
 
 model = TrOCR()
 
@@ -22,14 +21,14 @@ def handle_uploaded_file(file):
 
     time.sleep(0.5)
 
-    # Create reversed_images directory if it doesn't exist
-    reversed_dir = os.path.join(upload_dir, 'reversed_images')
-    os.makedirs(reversed_dir, exist_ok=True)
+    # Create output directory if it doesn't exist
+    output_dir = os.path.join(upload_dir, 'processed_text')
+    os.makedirs(output_dir, exist_ok=True)
 
     try:
         # Create output filename
-        output_filename = f"{os.path.splitext(file.name)[0]}_reversed{os.path.splitext(file.name)[1]}"
-        output_path = os.path.join(reversed_dir, output_filename)
+        output_filename = f"{os.path.splitext(file.name)[0]}_out.txt"
+        output_path = os.path.join(output_dir, output_filename)
 
         # Process the single uploaded file
         if model.perform_ocr(full_path, output_path):
