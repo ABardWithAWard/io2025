732429f79dba0e0cee2013aac2a703b8380be91f
Mateusz Wątły <95935287+ABardWithAWard@users.noreply.github.com>
2025-04-13 16:35:39 +0200

    merge

--- ocr/application/urls.py
+++ ocr/application/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'application'
+
 urlpatterns = [
     path('', views.upload_file, name='index'),
     path("api/files", views.get_files, name="get_files"),
