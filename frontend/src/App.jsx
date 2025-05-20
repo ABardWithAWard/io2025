074b96b868335b7a61abe7f3c380c0022a6af11f
Mateusz Wątły <95935287+ABardWithAWard@users.noreply.github.com>
2025-05-20 04:30:53 +0200

    Added mock page /profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavbarComponent from './components/NavbarComponent.jsx';
 import UploadPage from './components/UploadPage';
 import ContactPage from './components/ContactPage';
+import ProfileComponent from './components/ProfileComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
         <Routes>
           <Route path="/upload" element={<UploadPage />} />
           <Route path="/contact" element={<ContactPage />} />
-
+          <Route path="/profile" element={<ProfileComponent />} />
         </Routes>
       </div>
     </Router>
