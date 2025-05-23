169f3cbfad9819194bf4192d887e5d0e8043cc4d
kon-mtal <konradmstlr@gmail.com>
2025-05-23 14:40:45 +0200

    Modified EasyOCR to reflect current state of project

--- ocr/application/model/easyocr.py
+++ ocr/application/model/easyocr.py
@@ -6,20 +6,20 @@ class EasyOCR(ModelBase):
         super().__init__("EasyOCR")
         self.reader = easyocr.Reader(['en'])
 
-    def perform_ocr(self, input_path, output_path):
-        try:
-            result = self.reader.readtext(input_path)
-            chars_in_current_line = 0
-            max_line_width = 80
-            with open(output_path, "w") as f:
-                for entry in result:
-                    seq = entry[1]
-                    chars_in_current_line += len(seq)
-                    if chars_in_current_line > max_line_width:
-                        f.writelines(f"{seq}\n")
-                        chars_in_current_line = 0
-                    else:
-                        f.writelines(f"{seq} ")
-            return True
-        except Exception:
-            return False
+    def perform_ocr(self, input_path):
+        result = self.reader.readtext(input_path)
+
+        return result
+
+# might turn out useful after merging everything and moving on to output
+# chars_in_current_line = 0
+# max_line_width = 80
+# with open(output_path, "w") as f:
+#     for entry in result:
+#         seq = entry[1]
+#         chars_in_current_line += len(seq)
+#         if chars_in_current_line > max_line_width:
+#             f.writelines(f"{seq}\n")
+#             chars_in_current_line = 0
+#         else:
+#             f.writelines(f"{seq} ")
