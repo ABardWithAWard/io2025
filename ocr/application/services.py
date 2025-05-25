e08ce6719451761adb36e8d6f29c977a32b91dd1
kon-mtal <konradmstlr@gmail.com>
2025-05-25 10:45:42 +0200

    Add export to txt and docx

--- ocr/application/services.py
+++ ocr/application/services.py
@@ -40,3 +40,30 @@ def handle_uploaded_file(file):
         easy_result = easy_model.perform_ocr(input_path=full_path)
         print("EasyOCR results:")
         print(" ".join([result for result in easy_result["text_predictions"]]))
+
+def output_processed_as_txt(word_list, output_path):
+    chars_in_current_line = 0
+    max_line_width = 80
+    with open(output_path, "w") as f:
+        for word in word_list:
+            chars_in_current_line += len(word)
+            if chars_in_current_line > max_line_width:
+                f.writelines(f"{word}\n")
+                chars_in_current_line = 0
+            else:
+                f.writelines(f"{word} ")
+
+def output_processed_as_docx(word_list, output_path):
+    document = Document()
+    document.add_paragraph(" ".join(word_list))
+    document.save(output_path)
+
+if __name__ == "__main__":
+    word_list = ["God", "save", "our", "gracious", "Queen,", "Long", "live", "our", "noble", "Queen,",
+                 "God", "save", "the", "queen!", "Send", "her", "victorious,", "Happy", "and", "Glorious",
+                 "Long", "to", "reign", "over", "us;", "God", "save", "the", "Queen!"]
+
+    abs_output_dir = "/home/km/PycharmProjects/io2025/test_output"
+
+    output_processed_as_txt(word_list, abs_output_dir + "/output.txt")
+    output_processed_as_docx(word_list, abs_output_dir + "/output.docx")
