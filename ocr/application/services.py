7eb9b94e9752128e01de1f2355f9c9b9f5c8a67a
kon-mtal <konradmstlr@gmail.com>
2025-05-25 12:20:19 +0200

    Add image retrieval service from database

--- ocr/application/services.py
+++ ocr/application/services.py
@@ -1,5 +1,14 @@
 import os
 from pathlib import Path
+import base64
+from io import BytesIO
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+from google.cloud.firestore_v1 import FieldFilter
+
+from PIL import Image
+from typing import List
 from django.core.files.storage import FileSystemStorage
 
 from application.model.paddleocr import PaddleOCR
@@ -9,7 +18,9 @@ from application.utils import validate_image_brightness
 paddle_model = PaddleOCR()
 easy_model = EasyOCR()
 
-def prepare_file_hierarchy (file):
+firestore_db = None
+
+def prepare_file_hierarchy(file):
     """Takes uploaded file and returns directory where it is saved and its detected content"""
     upload_dir = os.path.abspath(os.environ['UPLOADED_FILES'])
     print(f"Upload directory: {upload_dir}")
@@ -38,3 +49,37 @@ def handle_uploaded_file(file):
         easy_result = easy_model.perform_ocr(input_path=full_path)
         print("EasyOCR results:")
         print(" ".join([result for result in easy_result["text_predictions"]]))
+
+def setup_firestore_db():
+    # There may be a more elegant solution, but for now I am using a global variable
+    global firestore_db
+
+    # The environment variable is pulled from rootdir/ocr/.env
+    credentials_obj = credentials.Certificate(os.environ["FIREBASE_KEY"])
+    firebase_admin.initialize_app(credentials_obj)
+    firestore_db = firestore.client()
+
+def retrieve_pictures_using_uid(desired_uid: str) -> List[Image]:
+    """
+    Retrieve a list of a user's PIL.Image objects from the Firestore database based on his UID.
+    Depends on the global (services.py) variable firebase_db being initialized. This variable is
+    initialized with setup_firestore_db (services.py) called within ApplicationConfig (apps.py).
+
+    :param desired_uid: The UID of the user for which pictures are to be retrieved from the db.
+    :return: A list of Pillow image objects generated from parsing image data stored in the db.
+    """
+
+    # A list of DocumentSnapshot objects meeting the userUid == desired_uid criterion
+    user_images_snapshot_list = (
+        firestore_db.collection("images")
+        .where(filter=FieldFilter("userUid", "==", desired_uid))
+        .stream()
+    )
+
+    # A list of user images encoded in b64 string format
+    user_images_b64_list = [user_image_snapshot.get("image_data") for user_image_snapshot in user_images_snapshot_list]
+
+    # A list of PIL.Image objects generated from decoding the b64 string format
+    user_images_list = [Image.open(BytesIO(base64.b64decode(user_image_b64))) for user_image_b64 in user_images_b64_list]
+
+    return user_images_list
