0aa6de8b0db97873b329001ba8aac23f118192a2
kon-mtal <konradmstlr@gmail.com>
2025-05-25 12:31:53 +0200

    Add docstring to setup function

--- ocr/application/services.py
+++ ocr/application/services.py
@@ -52,6 +52,9 @@ def handle_uploaded_file(file):
         print(" ".join([result for result in easy_result["text_predictions"]]))
 
 def setup_firestore_db():
+    """
+    Initialize the global firestore_db variable to hold a google.cloud.firestore_v1 Client object.
+    """
     # There may be a more elegant solution, but for now I am using a global variable
     global firestore_db
 
