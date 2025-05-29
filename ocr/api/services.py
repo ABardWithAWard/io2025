54c381ac757d6ed45bb5bdc8d54de6fb5be3c6c6
kon-mtal <konradmstlr@gmail.com>
2025-05-29 20:41:00 +0200

    Add image brightness validation

--- ocr/api/services.py
+++ ocr/api/services.py
@@ -12,6 +12,7 @@ from application.services import paddle_model, easy_model
 
 from application.model.paddleocr import PaddleOCR
 from application.model.easyocr import EasyOCR
+from application.utils import validate_image_brightness
 
 paddle_model = PaddleOCR()
 easy_model = EasyOCR()
@@ -41,39 +42,40 @@ def handle_uploaded_file(file, user_uid=None):
     """Takes file uploaded in form and calls helper function to manage file and its contents"""
     full_path = prepare_file_hierarchy(file)
 
-    paddle_result_list = paddle_model.perform_ocr(input_path=full_path)
-    print("PaddleOCR results:")
-    print(" ".join([result for result in paddle_result_list[0]["rec_texts"]]))
+    if validate_image_brightness(full_path):
+        paddle_result_list = paddle_model.perform_ocr(input_path=full_path)
+        print("PaddleOCR results:")
+        print(" ".join([result for result in paddle_result_list[0]["rec_texts"]]))
 
-    easy_result_list = easy_model.perform_ocr(input_path=full_path)
-    print("EasyOCR results:")
-    print(" ".join([result[1] for result in easy_result_list]))
+        easy_result_list = easy_model.perform_ocr(input_path=full_path)
+        print("EasyOCR results:")
+        print(" ".join([result[1] for result in easy_result_list]))
 
-    # Initialize Firebase if not already initialized
-    if not firebase_admin._apps:
-        cred_path = os.environ['FIREBASE_KEY']
-        if not os.path.exists(cred_path):
-            raise Exception('Firebase credentials not found')
-        cred = credentials.Certificate(cred_path)
-        firebase_admin.initialize_app(cred)
+        # Initialize Firebase if not already initialized
+        if not firebase_admin._apps:
+            cred_path = os.environ['FIREBASE_KEY']
+            if not os.path.exists(cred_path):
+                raise Exception('Firebase credentials not found')
+            cred = credentials.Certificate(cred_path)
+            firebase_admin.initialize_app(cred)
 
-    # Get Firestore client
-    db = firestore.client()
+        # Get Firestore client
+        db = firestore.client()
 
-    # Read and encode the image
-    with open(full_path, 'rb') as image_file:
-        encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+        # Read and encode the image
+        with open(full_path, 'rb') as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
 
-    # Add image to Firestore with userUid
-    image_ref = db.collection('images').document()
-    image_ref.set({
-        'image_data': encoded_string,
-        'filename': file.name,
-        'timestamp': firestore.SERVER_TIMESTAMP,
-        'userUid': user_uid  # This will be null for unauthenticated users
-    })
+        # Add image to Firestore with userUid
+        image_ref = db.collection('images').document()
+        image_ref.set({
+            'image_data': encoded_string,
+            'filename': file.name,
+            'timestamp': firestore.SERVER_TIMESTAMP,
+            'userUid': user_uid  # This will be null for unauthenticated users
+        })
 
-    # We can look up images using https://base64.guru/converter/decode/image
-    # As they are saved as base64 strings
+        # We can look up images using https://base64.guru/converter/decode/image
+        # As they are saved as base64 strings
 
-    return full_path
+        return full_path
