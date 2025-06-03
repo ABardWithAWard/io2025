15a86b5935a69435b38a6e33fd98a8d12b669ded
kon-mtal <konradmstlr@gmail.com>
2025-06-03 20:49:04 +0200

    Prepare for two languages, handle_uploaded_file now returns JSON

--- ocr/api/services.py
+++ ocr/api/services.py
@@ -2,10 +2,6 @@ import os
 import base64
 import json
 from io import BytesIO
-from typing import List
-
-from docx import Document
-from docx.shared import Pt
 from PIL import Image
 from django.core.files.storage import FileSystemStorage
 from django.core.files.uploadedfile import UploadedFile
@@ -22,6 +18,7 @@ paddle_model = PaddleOCR()
 easy_model = EasyOCR()
 
 DEBUG_MODE = False
+POLISH_MODE = False
 
 def get_files():
     """Get list of files from the upload directory"""
@@ -87,15 +84,17 @@ def handle_uploaded_file(file, user_uid=None):
     full_path = prepare_file_hierarchy(file)
 
     if validate_image_brightness(full_path):
-        paddle_result = paddle_model.perform_ocr(input_path=full_path)
-        if DEBUG_MODE:
-            print("PaddleOCR results:")
-            print(" ".join([result for result in paddle_result["text_predictions"]]))
+        if POLISH_MODE:
+            result = easy_model.perform_ocr(input_path=full_path)
+            if DEBUG_MODE:
+                print("EasyOCR results:")
+                print(" ".join([word for word in result["text_predictions"]]))
 
-        easy_result = easy_model.perform_ocr(input_path=full_path)
-        if DEBUG_MODE:
-            print("EasyOCR results:")
-            print(" ".join([result for result in easy_result["text_predictions"]]))
+        if not POLISH_MODE:
+            result = paddle_model.perform_ocr(input_path=full_path)
+            if DEBUG_MODE:
+                print("PaddleOCR results:")
+                print(" ".join([word for word in result["text_predictions"]]))
 
         # Initialize Firebase if not already initialized
         if not firebase_admin._apps:
@@ -124,44 +123,6 @@ def handle_uploaded_file(file, user_uid=None):
         # We can look up images using https://base64.guru/converter/decode/image
         # As they are saved as base64 strings
 
-        return full_path
+        json_ocr_result = convert_result_to_json(file, full_path, result)
 
-def output_processed_as_txt(word_list: List[str], output_path: str, line_width=80):
-    """
-    Output a list of words (strings) extracted from an image into a .txt file at output_path.
-    :param word_list: List of strings which are extracted words.
-    :param output_path: A string representing the output_path for the .txt file to be generated at.
-    :param line_width: The limit in characters after which a full new word cannot be written (but a part of the word may).
-    """
-    chars_in_current_line = 0
-    max_line_width = line_width
-    with open(output_path, "w") as f:
-        for word in word_list:
-            chars_in_current_line += len(word)
-            # If limit exceeded after writing this word, write
-            if chars_in_current_line > max_line_width:
-                # But write with a newline and reset next line to the beginning
-                f.writelines(f"{word}\n")
-                chars_in_current_line = 0
-            else:
-                # Otherwise simply write
-                f.writelines(f"{word} ")
-
-def output_processed_as_docx(word_list: List[str], output_path: str, font_size=11):
-    """
-    Output a list of words (strings) extracted from an image into a .docx file at output_path.
-    :param word_list: List of strings which are extracted words.
-    :param output_path: A string representing the output_path for the .txt file to be generated at.
-    :param font_size: The size of the font of the text which will take up the entire width at all times.
-    """
-    document = Document()
-
-    # Add_run is the most general "generate text" method. It gives access to a Font obj through .font
-    font = document.add_paragraph().add_run(" ".join(word_list)).font
-
-    # Through the Font obj we can modify the appearance of the text
-    font.name = "Arial"
-    font.size = Pt(font_size)
-
-    # The appearance will be saved
-    document.save(output_path)
+        return json_ocr_result
