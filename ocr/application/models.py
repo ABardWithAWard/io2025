274c2a5a3400d742ac542187e53ab69bdc8fd484
teYdemaNtoN <florson2002@gmail.com>
2025-06-08 20:05:19 +0200

    Old admin models deleted

--- ocr/application/models.py
+++ ocr/application/models.py
@@ -1,17 +1,5 @@
 from django.db import models
 
-class FirebaseDataLimit(models.Model):
-    class Meta:
-        managed = False
-        verbose_name = "Limit danych"
-        verbose_name_plural = "Limit danych"
-
-class FirebaseFileLimit(models.Model):
-    class Meta:
-        managed = False
-        verbose_name = "Limit plików"
-        verbose_name_plural = "Limit plików"
-
 # Model to store uploaded files
 class UploadedFile(models.Model):
     file = models.FileField(upload_to='uploads/')
